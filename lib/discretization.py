72d99acf314b4e30dd3ffa0ff83c495fad6ec0a0
cclauss <cclauss@bluewin.ch>
2018-07-21 03:38:15 +0200

    from six.moves import xrange

--- lib/discretization.py
+++ lib/discretization.py
@@ -18,6 +18,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from functools import partial
+from six.moves import xrange
 import tensorflow as tf
 from tensorflow.python.training import moving_averages
 
